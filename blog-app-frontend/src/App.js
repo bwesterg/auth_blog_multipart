20530009e185843ce85e1cf8c9229aa1de1bf5b5
bwesterg <benton.westergaard@gmail.com>
2021-09-15 13:30:26 -0600

    front end and back end now connected, bicycles are currently in state.

--- blog-app-frontend/src/App.js
+++ blog-app-frontend/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+const BASEURL = "http://localhost:3000/bicycles";
 
-class App extends Component() {
+class App extends Component {
   
   state = {
     bicycles: []
   }
 
+  componentDidMount(){
+    this.getBicycles()
+  }
+
+  getBicycles = () => {
+    fetch(BASEURL)
+      .then(response => response.json())
+      .then(bicycles => this.setState({bicycles}))
+  }
+
   render(){
     return (
       <div className="App">
-      <header className="App-header">
         <h1>Bicycle App</h1>
-      </header>
     </div>
     );
   }
