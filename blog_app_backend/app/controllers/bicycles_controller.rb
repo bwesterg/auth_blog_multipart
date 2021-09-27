9cd116283b8dbafd82383ab7129088d449490346
bwesterg <benton.westergaard@gmail.com>
2021-09-27 15:03:18 -0600

    New bicycles now persist.

--- blog_app_backend/app/controllers/bicycles_controller.rb
+++ blog_app_backend/app/controllers/bicycles_controller.rb
@@ -5,4 +5,17 @@ class BicyclesController < ApplicationController
         render json: @bicycles
     end
 
+
+    def create
+        @bicycle = Bicycle.create(
+            usage: params[:usage],
+            frame_material: params[:frame_material],
+            manufacturer: params[:manufacturer],
+            speeds: params[:speeds]
+        )
+
+        render json: @bicycle, status: :created
+    end
 end
+
+
