abcb9ba45b058880ee2336929857c4d99d975d4c
bwesterg <benton.westergaard@gmail.com>
2021-09-27 14:10:36 -0600

    Saving work, in progress of connecting form.

--- blog-app-frontend/src/components/BicycleItem.js
+++ blog-app-frontend/src/components/BicycleItem.js
@@ -3,10 +3,12 @@ import React from 'react';
 export default function BicycleItem({usage, frame_material, manufacturer, speeds}) {
     return(
         <li className="bicycle-item">
-            <h4>{usage}</h4>
-            <h4>{frame_material}</h4>
-            <h4>{manufacturer}</h4>
-            <h4>{speeds}</h4>
+            <h4>Type: {usage}</h4>
+            <h4>Frame Material: {frame_material}</h4>
+            <h4>Made by: {manufacturer}</h4>
+            <h4>Speeds: {speeds}</h4>
         </li>
     )
 }
+
+// test124 (for git issue)
